125ce4814bdf9d25d3d895df173374c6c91be123
jahsper Harrell <jahsper.harrell@gmail.com>
2023-07-27 20:01:20 -0600

    Fixed all of the errors for the signupForm.js.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -53,9 +53,10 @@ const SignupForm = () => {
 
   return (
     <>
-      {/* This is needed for the validation functionality above /}
+      {/* This is needed for the validation functionality above */}
+
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        {/ show alert if server response is bad */}
+        {/* show alert if server response is bad */}
         <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
           Something went wrong with your signup!
         </Alert>
